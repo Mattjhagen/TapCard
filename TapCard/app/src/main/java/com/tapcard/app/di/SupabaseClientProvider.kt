890628eed5a6d60763dcfa66222d4bba2f2c02cd
Matty Hagen <matty@p3lending.space>
2026-07-12 18:24:45 -0500

    fix(android): install Storage plugin to resolve profile photo upload crash

--- TapCard/app/src/main/java/com/tapcard/app/di/SupabaseClientProvider.kt
+++ TapCard/app/src/main/java/com/tapcard/app/di/SupabaseClientProvider.kt
@@ -5,6 +5,7 @@ import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
 
 object SupabaseClientProvider {
     val client: SupabaseClient? by lazy {
@@ -18,6 +19,7 @@ object SupabaseClientProvider {
                     host = "login-callback"
                 }
                 install(Postgrest)
+                install(Storage)
             }
         } else {
             null
